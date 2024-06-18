1090fe0d5dc9c8692b0cae77782d886c4f2f2c2e
kirthiswaran24 <kirthiswarang@infographicanalytics.com>
2024-06-18 10:45:49 +0530

    fiest push...

---
+++ src/handlers/firstemployee.js
@@ -0,0 +1,20 @@
+// src/handlers/firstemployee.lambdaHandler.js
+
+exports.lambdaHandler = async (event) => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Hello from FirstEmployeeFunction!'
+            })
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: 'Internal Server Error'
+            })
+        };
+    }
+};
